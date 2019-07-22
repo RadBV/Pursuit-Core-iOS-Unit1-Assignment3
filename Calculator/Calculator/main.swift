9053d0ce91cdf63d5ab0485f41f17216e6a001db
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-07-22 17:14:23 -0400

    made some calculator code stuff

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -2,23 +2,64 @@
 //  main.swift
 //  Calculator
 //
-//  Created by Alex Paul on 10/25/18.
-//  Copyright © 2018 Pursuit. All rights reserved.
+//  Created by Radharani Ribas-Valongo on 7/22/19.
+//  Copyright © 2019 Radharani Ribas-Valongo. All rights reserved.
 //
 
 import Foundation
 
-func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
-  switch opString {
-  case "+":
-    return {x, y in x + y }
-  case "-":
-    return {x, y in x - y }
-  case "*":
-    return {x, y in x * y }
-  case "/":
-    return {x, y in x / y }
-  default:
-    return {x, y in x + y }
-  }
+var operations: ([String: (Double, Double) -> Double]) = ["+": { $0 + $1 },
+                                                          "-": { $0 - $1 },
+                                                          "*": { $0 * $1 },
+                                                          "/": { $0 / $1 }]
+print("do something")
+
+var again = ""
+var calculatorWorking = true
+
+func calculator() {
+    while calculatorWorking == true {
+        var input = readLine()
+        var calculation = ""
+        var calculationNumbers: [Double] = []
+
+            if let theInputUnwrapped = input {
+                calculation += theInputUnwrapped
+        }
+        var calculationArray = calculation.components(separatedBy: " ")
+
+        var doublesArray: [Double] = []
+        var operatorSign = ""
+
+        doublesArray.append(Double(calculationArray[0])!)
+        doublesArray.append(Double(calculationArray[2])!)
+        operatorSign = calculationArray[1]
+
+        var reducedAnswer = 0.0
+        func doOperator(x:[Double]) -> Double {
+            for (key,value) in operations {
+                if operatorSign == key {
+                    reducedAnswer = x.reduce(0, value)
+                }
+            }
+            return reducedAnswer
+        }
+
+        let answer = doOperator(x: doublesArray)
+        
+        let rightAnswers = ["the answer is probably \(answer), idk tho :/", "thine answer hast been revealeth. your lucky numbereth is \(answer)", "it's \(answer)", "u don't even know what \(calculation)? that's okay, i'm not good at math either. (it's \(answer))"]
+
+        print(rightAnswers.randomElement()!)
+        
+        print("do you want to keep making me do your math? if yes, enter any character. if no, enter 'n'")
+        
+        if let continueAnswer = input {
+            again = continueAnswer
+            if again == "n" {
+                break
+            }
+        }
+    }
 }
+
+calculator()
